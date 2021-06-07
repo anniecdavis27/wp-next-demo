b05243b498499fe507d11fd97f80a7fdd7866936
Annie Davis <annie-davis@Annies-MacBook-Air.local>
2021-06-07 13:51:57 -0400

    adding custom post types

--- pages/extrademopage.tsx
+++ pages/extrademopage.tsx
@@ -1,63 +1,60 @@
 import { useQuery, gql } from "@apollo/client";
 
-    // const authorsQuery = gql`
-    // {
-    //     users {
-    //       nodes {
-    //         firstName
-    //         lastName
-    //         roles {
-    //           nodes {
-    //             displayName
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }      
-    // `;
-
-    const announcementsQuery = gql`
+    const authorsQuery = gql`
     {
-      announcements {
-        nodes {
-          date
-          title
-          content
+        users {
+          nodes {
+            firstName
+            lastName
+            roles {
+              nodes {
+                displayName
+              }
+            }
+          }
         }
-      }
-    }    
+      }      
     `;
+
+    // const announcementsQuery = gql`
+    // {
+    //   announcements {
+    //     nodes {
+    //       date
+    //       title
+    //       content
+    //     }
+    //   }
+    // }    
+    // `;
 
 
 const ExtraDemoPage = () => {
-    const { data } = useQuery(announcementsQuery)
+    const { data } = useQuery(authorsQuery)
     // const { datab } = useQuery(announcementsQuery)
 
-    
-    // const authors = data?.users?.nodes ?? [];
-    // console.log(authors)
-
-    const announcements = data?.announcements?.nodes ?? [];
-    console.log(announcements)
+    const authors = data?.users?.nodes ?? [];
+    // const announcements = data?.announcements?.nodes ?? [];
+    // console.log(announcements)
     return (
 
     <>
     <h1>Extra Demo Page</h1>;
     <h3>It is much easier to create custom pages with custom features</h3>
 
-    {/* <ol>
+    <ol>
         {authors.map((author:any) => <li>
             <p>{author.firstName} {author.lastName}</p>
         </li>)}
-    </ol> */}
+    </ol>
     
-    <ul>
+    {/* <ul>
       {announcements.map((announcement: any) => <li>
             <h3>{announcement.title}</h3>
             <p>{announcement.content}</p>
             <p>{announcement.date}</p>
         </li>)}
-    </ul>
+    </ul> */}
     </>
     )
 
